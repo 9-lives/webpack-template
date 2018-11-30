ad82a0f77e8aad273ff833104246040b4d189257
彭泽华 <pengzehua@huawei.com>
2018-11-30 19:19:56 +0800

    [styles] add _normalize.scss ;

--- build/build.conf.js
+++ build/build.conf.js
@@ -6,11 +6,11 @@ const path = require('path')
 module.exports = {
   assetsDir: 'assets/', // 静态资源目录
   ctx: `${path.join(__dirname, '..')}/`, // 编译配置文件上下文。使配置文件独立于工程根目录
-  stylesDir: 'styles/', // styles 目录
   htmlDir: 'html/', // HTML 目录
   imgsSubDir: 'imgs/', // 静态资源目录-图片资源子目录
   maxSzToBase64: 3072, // 低于此大小的图片可能被转换为 base64 编码
   optPath: 'dist/', // 文件输出路径
   publicDir: 'public/', // 静态资源复制目录
   srcDir: 'src/', // 源码目录
+  stylesDir: 'styles/', // styles 目录
 }
--- src/styles/index.js
+++ src/styles/index.js
@@ -1,1 +1,4 @@
-export * from 'normalize.css'
+// 库
+import 'normalize.css'
+// 自定义样式
+import 'styles/common/_normalize.scss'
